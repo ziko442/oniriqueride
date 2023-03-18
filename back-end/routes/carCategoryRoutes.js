a9de90bc64ed141d938b58d6415f5b1b212b2259
ziko442 <ziko442@gmail.com>
2023-03-18 18:12:44 +0100

    update some routers

--- back-end/routes/carCategoryRoutes.js
+++ back-end/routes/carCategoryRoutes.js
@@ -3,31 +3,24 @@ const router = express.Router();
 
 const upload = require('../middleware/upload-images');
 
-const {
-  createCarCategory,
-  getCarCategories,
-  getCarCategoryById,
-  updateCarCategoryById,
-  deleteCarCategoryById,
-  uploadCarCategoryImage
-} = require('../controllers/carCategoryController');
+const carCategoryController = require('../controllers/carCategoryController');
 
 // Create a new car category
-router.post('/', createCarCategory);
+router.post('/', carCategoryController.createCarCategory);
 
 // Get all car categories
-router.get('/', getCarCategories);
+router.get('/', carCategoryController.getCarCategories);
 
 // Get a single car category by ID
-router.get('/:id', getCarCategoryById);
+router.get('/:id', carCategoryController.getCarCategoryById);
 
 // Update a car category by ID
-router.put('/:id', updateCarCategoryById);
+router.put('/:id', carCategoryController.updateCarCategoryById);
 
 // Delete a car category by ID
-router.delete('/:id', deleteCarCategoryById);
+router.delete('/:id', carCategoryController.deleteCarCategoryById);
 
 // Upload an image for a car category by ID
-router.post('/:id/image', upload.single('image'), uploadCarCategoryImage);
+router.post('/:id/image', upload.single('image'), carCategoryController.uploadCarCategoryImage);
 
 module.exports = router;
