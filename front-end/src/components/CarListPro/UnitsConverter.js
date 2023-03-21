321942fc622fd1a168c62d4859c6cce341880679
ziko442 <ziko442@gmail.com>
2023-03-21 20:01:56 +0000

    fix errors

--- front-end/src/components/CarListPro/UnitsConverter.js
+++ front-end/src/components/CarListPro/UnitsConverter.js
@@ -3,8 +3,8 @@
 
 export function UnitsConverter(distance, duration) {
   // Replace this with your own pricing algorithm
-  const pricePerMile = 4;
-  const pricePerMinute = 1.3;
+  // const pricePerMile = 4;
+  // const pricePerMinute = 1.3;
   const miles = distance / 1609.34;
   const minutes = duration / 60;
   // const price = (miles * pricePerMile) + (minutes * pricePerMinute);
--- front-end/src/components/CarListPro/index.js
+++ front-end/src/components/CarListPro/index.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
+// import { Link } from "react-router-dom";
 // import { useTranslation } from "react-i18next";
 import { Container, Row, Col } from "react-bootstrap";
 
