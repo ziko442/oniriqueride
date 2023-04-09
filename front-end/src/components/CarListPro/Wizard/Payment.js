8c6736e19b548dbf96d023e00bf9c00c2cb54b6b
ziko442 <ziko442@gmail.com>
2023-04-09 17:55:52 +0000

    add some fields to the payment function

--- front-end/src/components/CarListPro/Wizard/Payment.js
+++ front-end/src/components/CarListPro/Wizard/Payment.js
@@ -1,61 +1,67 @@
-// Dependencies
-import * as React from 'react';
-import { CreditCard, PaymentForm } from 'react-square-web-payments-sdk';
+import * as React from "react";
+import { Button } from "react-bootstrap";
+import { CreditCard, PaymentForm } from "react-square-web-payments-sdk";
+import axios from "axios"; // Import axios library for making API requests
 
-const Payment = () => (
-  <PaymentForm
-    /**
-     * Identifies the calling form with a verified application ID generated from
-     * the Square Application Dashboard.
-     */
-    applicationId="sandbox-sq0idb-dR9X9p5XPSarc6SaSOvHGg"
-    /**
-     * Invoked when payment form receives the result of a tokenize generation
-     * request. The result will be a valid credit card or wallet token, or an error.
-     */
-    cardTokenizeResponseReceived={(token, buyer) => {
-      console.info({ token, buyer });
-    }}
-    /**
-     * This function enable the Strong Customer Authentication (SCA) flow
-     *
-     * We strongly recommend use this function to verify the buyer and reduce
-     * the chance of fraudulent transactions.
-     */
-    createVerificationDetails={() => ({
-      amount: '1.00',
-      /* collected from the buyer */
+const Payment = ({ previousStep, nextStep }) => {
+  // Function to handle card tokenize response
+  const handleCardTokenizeResponse = async (token, buyer) => {
+    // Create an order object with the order details
+    const order = {
+      amount: "1.00", // Update with your desired amount
       billingContact: {
-        addressLines: ['123 Main Street', 'Apartment 1'],
-        familyName: 'Doe',
-        givenName: 'John',
-        countryCode: 'GB',
-        city: 'London',
+        addressLines: ["123 Main Street", "Apartment 1"],
+        familyName: "Doe",
+        givenName: "John",
+        countryCode: "GB",
+        city: "London",
       },
-      currencyCode: 'GBP',
-      intent: 'CHARGE',
-    })}
-    /**
-     * Identifies the location of the merchant that is taking the payment.
-     * Obtained from the Square Application Dashboard - Locations tab.
-     */
-    locationId="LTW11K871DYQA"
-  >
-    <CreditCard
-      /**
-       * Attach the CreditCard component to the form and provide the billing information fields.
-       */
-      cardForm={() => ({
-        billingContactFields: {
-          postalCode: true,
-          country: true,
-          addressLines: true,
-          locality: true,
-          region: true,
-        },
-      })}
-    />
-  </PaymentForm>
-);
+      currencyCode: "GBP",
+      intent: "CHARGE",
+      token: token, // Pass the token received from cardTokenizeResponseReceived
+    };
+
+    try {
+      // Send a POST request to the backend with the order details
+      const response = await axios.post("/api/payments", order); // Update with your backend API endpoint
+      // Handle the response from the backend
+      console.log("Payment successful", response.data);
+      // Update the UI or take any other necessary action based on the response
+      // E.g., show a success message, navigate to the next step, etc.
+    } catch (error) {
+      // Handle any errors that occurred during the API call
+      console.log(order);
+      console.error("Payment failed", error);
+      // Update the UI or take any other necessary action based on the error
+      // E.g., show an error message, retry payment, etc.
+    }
+  };
+
+  return (
+    <div>
+      <PaymentForm
+        applicationId="sandbox-sq0idb-dR9X9p5XPSarc6SaSOvHGg"
+        cardTokenizeResponseReceived={handleCardTokenizeResponse} // Pass the function to handle card tokenize response
+        createVerificationDetails={() => ({
+          amount: "1.00",
+          billingContact: {
+            addressLines: ["123 Main Street", "Apartment 1"],
+            familyName: "Doe",
+            givenName: "John",
+            countryCode: "GB",
+            city: "London",
+          },
+          currencyCode: "GBP",
+          intent: "CHARGE",
+        })}
+        locationId="LTW11K871DYQA"
+      >
+        <CreditCard>
+          <>Pay 12 USD</>
+        </CreditCard>
+      </PaymentForm>
+    </div>
+  );
+};
 
 export default Payment;
